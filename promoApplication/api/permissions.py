86e1013f3d35fc6a00c674706d4e541f7efe4416
mohamed awad <awadmohamed233@gmail.com>
2020-10-03 02:39:37 +0200

    New: add new permission called IsOwner

--- promoApplication/api/permissions.py
+++ promoApplication/api/permissions.py
@@ -10,3 +10,11 @@ class IsAdmin(permissions.BasePermission):
         current_user = User.objects.get(username=request.user)
         return current_user.is_admin
 
+
+class IsOwner(permissions.BasePermission):
+    """
+    Object-level permission to only allow normal user for his own promo data.
+    """
+    def has_object_permission(self, request, view, obj):
+        current_user = User.objects.get(username=request.user)
+        return current_user.user == obj.user
